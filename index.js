991d0d15d56b334dc1b1dff353427feddc0d10f3
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-19 20:57:25 +0300

    dog class

---
+++ index.js
@@ -0,0 +1,8 @@
+const Dog = require('./js/Dog');
+
+const rexas = new Dog('Rex', 'brown');
+const spaikas = new Dog('Spike', 'white');
+
+
+console.log(rexas.hasTale);
+console.log(spaikas.hasTale);
